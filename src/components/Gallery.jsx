ddba505e9d4369a5461bff202f88b0f605b9a181
Tansuani Waring <tansuanigomezw@gmail.com>
2023-10-03 22:35:59 -0300

    intention to show pictures

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,25 @@
+
+
+
 const Gallery = () => {
+
+  const [picture, setPicture] = useState([])
+    const gatherPicture = async () => {
+      const url = <link rel="stylesheet" href=".../public/photos/" /> //está bien?
+      const answer = await fetch (url)
+      const data = await answer.json()
+      const spread = data.docs.map((image) => {
+        return {
+          id: image._id,
+          url: image._url,
+        }
+      })
+    .sort((a, b) => a.id.localeCompare(b.id));
+    setPicture(spread)
+    }
+    
+
+
   return <div className="gallery grid-columns-5 p-3"></div>;
 };
 export default Gallery;
