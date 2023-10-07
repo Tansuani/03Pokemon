90b0ff5229b4e9b1391c273386fb3036bc4620bc
Tansuani Waring <tansuanigomezw@gmail.com>
2023-10-07 15:43:42 -0300

    sintaxis correction and favorite resolving

--- src/Context/PictureContext.jsx
+++ src/Context/PictureContext.jsx
@@ -1,34 +1,39 @@
 
-import {createContext, useState} from 'react';
+import { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const PictureContext = createContext()
 
-const URL = "./photos.json"
-const PictureProvider = ({children}) => {
+const URL = "/photos.json"
+
+const PictureProvider = ({ children }) => {
     const [picture, setPicture] = useState([]);
+
     const gatherPicture = async () => {
-        try{
-            const response =await axios.get(URL);
-            if(!response.status) {
+        try {
+            const response = await axios.get(URL);
+            if (!response.status) {
                 throw new Error("Error. Sin datos");
             }
-            const {picture: spread} = response.data;
-            setPicture(spread.map((image)=>({...image, ifFavorite: false
+            const { photos: dataPictures } = response.data;
+            setPicture(dataPictures.map((image) => ({
+                ...image, ifFavorite: false
             })));
-    } catch (error) {
+        } catch (error) {
+            console.log("falla de api" + error)
+        }
     }
+    useEffect(() => {
+        gatherPicture();
+    }, []);
+
+    return (
+        <PictureContext.Provider
+            value={{ picture, setPicture, }}>
+            {children}
+        </PictureContext.Provider>
+    );
 };
 
-useEffect(() => {
-    gatherPicture();
-}, []);
 
-return(
-    <PictureContext.Provider
-    value={{picture, setPicture,}}>
-        {children}
-    </PictureContext.Provider>
-);
-}
 export default PictureProvider;
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,35 +1,37 @@
 import { useContext } from "react";
-import { PictureContext } from "./Context/PictureContext";
+import { PictureContext } from "../Context/PictureContext";
 import IconHeart from './IconHeart';
 
 const Gallery = () => {
-  const {picture, setPictures } = useContext(PictureContext);
+  const { picture, setPicture } = useContext(PictureContext);
+
   const like = (id) => {
     const newPicture = picture.map((image) => {
-      if(image.id === id) {
-        return {...image, ifFavorite: !image.ifFavorite,};
+      if (image.id === id) {
+        return { ...image, ifFavorite: !image.ifFavorite };
       }
       return image;
     });
-    setPictures(newPicture);
+    setPicture(newPicture);
   };
 
   return (
     <div className="gallery grid-columns-5 p-3">
-      {photos.map((image, 1) = (
+      {picture.map((image, i) => (
         <div
-          key={1}
-          onClick={()=> like(image.id)}
+          key={i}
+          onClick={() => like(image.id)}
           className="photo"
           style={{ backgroundImage: `url(${image.src.medium})` }}>
-            <IconHeart filled={image.ifFavorite} /> {' '}
-            <section>
-              <p>{image.alt}</p>
-            </section>
+
+          <IconHeart filled={image.ifFavorite} />
+          <section>
+            <p>{image.alt}</p>
+          </section>
         </div>
       ))}
     </div>
-    
+
   );
 };
 export default Gallery;
