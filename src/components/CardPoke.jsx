395f530db223eb921a73ee26d907cae523f98e44
Tansuani Waring <tansuanigomezw@gmail.com>
2023-10-18 20:56:48 -0300

    modifying code to comply with task

---
+++ src/components/CardPoke.jsx
@@ -0,0 +1,54 @@
+import {Card} from 'react-bootstrap'
+import { useNavigate} from 'react-router-dom'
+
+const CardPoke = ({ pokemon}) =>{
+const { name, stats, src, types } = pokemon
+
+const navigate = useNavigate()
+
+const handleClick = () => {
+    navigate(`/pokemons`)
+}
+
+return(
+    <>
+    <section className='container text-center'>
+        <Card
+        style={{width: '37rem'}}
+        className="mt -5 mx-auto text-center shadow-lg p-3 nb-5 bg-dark rounded text-white"
+        >
+        <Card.Img
+        height="300"
+        src={src}
+        />
+        <Card.Body>
+            <Card.Title className= "fw-bold"> {name} </Card.Title>
+            <span className="list-unstyled text-start">
+                <Card.Text className=" fw-bold text-center">
+                   {stats?.map((stat,i) => (
+                    <li key={i}>
+                        {stat.name}: {stat.base}
+                    </li>
+                   ))} 
+                </Card.Text>
+            </span>
+            <Card.Text className="bg- danger text-while p-2 rounded fw-bold text-center text-capitalize mt-3">
+             {types}
+            </Card.Text>
+        </Card.Body>
+        </Card>
+        <button
+        onClick= {handleClick}
+        className="btn btn-info btn-lg btn-block mt-1"
+        >
+            back
+        </button>
+    </section>
+    </>
+
+    
+
+)
+
+}
+export default CardPoke
---
+++ src/components/Menu.jsx
@@ -0,0 +1,40 @@
+import { Navbar } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+const Menu = () => {
+    const activeClass = ({ isActive }) => (isActive ? "active" : undefined)
+
+    return (
+        <Navbar
+            className='d-flex justify-content-between px-5'
+            variant='light'
+            style={{ background: '#58585a' }}
+        >
+            <Navbar.Brand>
+                <img
+                width="50"
+                src="/assets/img/poke-icon.jpg"
+                alt=""
+                />
+            </Navbar.Brand>
+            <div>
+                <NavLink
+                className={activeClass + "m-3"}
+                to="/"
+                >
+                    Home
+                </NavLink>
+
+                <NavLink
+                className={activeClass}
+                to="/pokemons"
+                >
+                    Pokemones
+                </NavLink>
+            </div>
+        </Navbar>
+    )
+
+}
+
+export default Menu
